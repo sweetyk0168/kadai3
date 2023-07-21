31fec3b3c250abf3c078c1e5cd3a118238d68480
EC2 Default User <ec2-user@ip-172-31-3-147.ap-northeast-1.compute.internal>
2023-07-21 13:00:18 +0000

    20230717_アプリケーションを完成させよう２の13章まで完了

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,13 @@
 class HomesController < ApplicationController
   def top
   end
+  
+  def about
+  end
+  
+  def create
+  end
+  
+  def show
+  end
 end
---
+++ app/controllers/post_images_controller.rb
@@ -0,0 +1,37 @@
+class PostImagesController < ApplicationController
+  #コントローラーの名前は複数形に設定する
+  def new
+    @post_image = PostImage.new
+  end
+  #投稿データの.保存
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_images_path
+  end
+
+  def index
+    @post_images = PostImage.all
+  end
+
+  def show
+    @post_image = PostImage.find(params[:id])
+  end
+  
+  def destroy
+    #データ（レコード）を1件取得
+    post_image = PostImage.find(params[:id])
+    #データ（レコード）を削除
+    post_image.destroy
+    #投稿一覧画面へリダイレクト
+    redirect_to '/post_images'
+  end
+  
+  #投稿データのストロングパラメータ
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+  end
+end
